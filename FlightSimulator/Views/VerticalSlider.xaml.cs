4ce2acf6df99ab2da385f656ef13cd95007d56b4
Shaked Cohen <shkcohen@gmail.com>
2019-04-09 19:32:21 +0300

    finished basic view

--- FlightSimulator/Views/VerticalSlider.xaml.cs
+++ FlightSimulator/Views/VerticalSlider.xaml.cs
@@ -10,28 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FlightSimulator
+namespace FlightSimulator.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for VerticalSlider.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class VerticalSlider : UserControl
     {
-        public MainWindow()
+        public VerticalSlider()
         {
             InitializeComponent();
-        }
-
-        private void FlightBoard_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void VerticalSlider_Loaded(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
