010512241bb619e79bad0b03afc11543862efee1
ronbenshimol <ronbenshimol@gmail.com>
2019-04-17 15:08:59 +0300

    server 1

--- FlightSimulator/MainWindow.xaml.cs
+++ FlightSimulator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlightSimulator.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,9 @@ namespace FlightSimulator
         public MainWindow()
         {
             this.DataContext = new ViewModels.MainWindowVM();
+
+            InfoServer server = InfoServer.Instence;
+            server.start();
         }
 
         
--- FlightSimulator/Model/InfoServer.cs
+++ FlightSimulator/Model/InfoServer.cs
@@ -1,20 +1,22 @@
 ﻿using FlightSimulator.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FlightSimulator.Model
 {
     class InfoServer :BaseNotify, IDisposable
     {
-        private int listenPort = 5400;
-        private const string serverIp = "127.0.0.1";
+        
         private TcpClient client;
         TcpListener listener;
+        object _syncLock = new object();
 
         private float lat;
         public float Lat { get { return lat; } set { lat = value; NotifyPropertyChanged("Lat"); } }
@@ -25,8 +27,9 @@ namespace FlightSimulator.Model
 
         private InfoServer()
         {
-            startListen();
+
         }
+
         private static InfoServer instance = null;
         public static InfoServer Instence
         {
@@ -39,9 +42,12 @@ namespace FlightSimulator.Model
         }
 
 
-        private void startListen()
+        public void start()
         {
+            int listenPort = ApplicationSettingsModel.Instance.FlightInfoPort;
+            string serverIp = ApplicationSettingsModel.Instance.FlightServerIP;
             IPAddress localAdd = IPAddress.Parse(serverIp);
+
             listener = new TcpListener(localAdd, listenPort);
             Console.WriteLine("Listening...");
             listener.Start();
@@ -49,8 +55,27 @@ namespace FlightSimulator.Model
             client = listener.AcceptTcpClient();
 
             Console.WriteLine("client connected!");
+            
+            Thread serverLitenerThread = new Thread(() =>
+            {
+
+                using (StreamReader reader = new StreamReader(client.GetStream(), Encoding.UTF8))
+                {
+                    lock(_syncLock)
+                    {
+                        
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            Console.WriteLine("server: " + line);
+                        }
+                    }
+                }
 
 
+            });
+
+            serverLitenerThread.Start();
         }
 
         public void Dispose()
